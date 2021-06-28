41fc7916e613de8fe8bb81157d4c887d02f787b2
Saidev Lakkakula <lakkakula.saidev@gmail.com>
2021-06-28 23:49:43 +0200

    Implemented hooks in Sidebar and Songs components....

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -1,12 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MdFavorite } from "react-icons/md";
 import { add_Favorite, remove_Favorite } from "../actions/favorites";
-import { connect } from "react-redux";
+import { connect, useSelector, useDispatch } from "react-redux";
 import { add_Queue, current_Song, add_Queue_Image } from "../actions/queue";
 
-const mapStateToProps = (state) => state;
-
-const mapDispatchToProps = (dispatch) => ({
+/* const mapDispatchToProps = (dispatch) => ({
     add_Favorite: (data) => {
         dispatch(add_Favorite(data));
     },
@@ -22,47 +20,43 @@ const mapDispatchToProps = (dispatch) => ({
     current_Song: (data) => {
         dispatch(current_Song(data));
     }
-});
+}); */
 
-class Song extends React.Component {
-    state = {
-        selected: false
+const Song = (props) => {
+    const { albumImage, songs, track } = { ...props };
+    const [selected, setSelected] = useState(false);
+
+    const state = useSelector((state) => state);
+    const dispatch = useDispatch();
+
+    const handleFavorite = (Track) => {
+        if (!selected) {
+            dispatch(add_Favorite(Track));
+            setSelected(true);
+        } else {
+            dispatch(remove_Favorite(Track));
+            setSelected(false);
+        }
     };
 
-    handleFavorite(Track) {
-        if (!this.state.selected) {
-            this.props.add_Favorite(Track);
-            this.setState({ selected: true });
-        } else {
-            this.props.remove_Favorite(Track);
-            this.setState({ selected: false });
-        }
-    }
-
-    handleQueue(albumImage, songs, currentSongId) {
-        this.props.add_Queue_Image(albumImage);
-        this.props.add_Queue(songs);
+    const handleQueue = (albumImage, songs, currentSongId) => {
+        dispatch(add_Queue_Image(albumImage));
+        dispatch(add_Queue(songs));
         let index = songs.findIndex((x) => x.id === currentSongId);
-        this.props.current_Song(index);
-    }
+        dispatch(current_Song(index));
+    };
 
-    render() {
-        return (
-            <div className="py-3 this.props.trackHover">
-                <MdFavorite className="cursor" onClick={() => this.handleFavorite(this.props.track)} style={{ color: this.state.selected ? "red" : "white" }} />
-                <span
-                    className="card-title this.props.trackHover px-3"
-                    onClick={() => this.handleQueue(this.props.albumImage, this.props.songs, this.props.track.id)}
-                    style={{ color: "white", cursor: "pointer" }}
-                >
-                    {this.props.track.title}
-                </span>
-                <small className="duration" style={{ color: "white" }}>
-                    {Math.floor(parseInt(this.props.track.duration) / 60)}:
-                    {parseInt(this.props.track.duration) % 60 < 10 ? "0" + (parseInt(this.props.track.duration) % 60) : parseInt(this.props.track.duration) % 60}
-                </small>
-            </div>
-        );
-    }
-}
-export default connect(mapStateToProps, mapDispatchToProps)(Song);
+    return (
+        <div className="py-3 this.trackHover">
+            <MdFavorite className="cursor" onClick={() => handleFavorite(track)} style={{ color: selected ? "red" : "white" }} />
+            <span className="card-title this.trackHover px-3" onClick={() => handleQueue(albumImage, songs, track.id)} style={{ color: "white", cursor: "pointer" }}>
+                {track.title}
+            </span>
+            <small className="duration" style={{ color: "white" }}>
+                {Math.floor(parseInt(track.duration) / 60)}:{parseInt(track.duration) % 60 < 10 ? "0" + (parseInt(track.duration) % 60) : parseInt(track.duration) % 60}
+            </small>
+        </div>
+    );
+};
+
+export default Song;
